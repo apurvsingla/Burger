3830b629ac984b3b338d850318215c0ef1bab0a4
Apurv <apurvsingla@gmail.com>
2020-07-25 17:28:10 +0530

    s

--- src/Components/Navigation/SideDrawer/SideDrawer.js
+++ src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Auxillary';
 import './SideDrawer.css';
 import Backdrop from '../../Ui/Backdrop/Backdrop';
 
